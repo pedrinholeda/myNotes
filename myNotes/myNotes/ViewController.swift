7d9ab67328739b7a57c1fc1e99846a4b6325109d
Pedro Leda <pedro.henrique.leda@gmail.com>
2021-09-02 22:23:26 -0300

    save notes

--- myNotes/myNotes/ViewController.swift
+++ myNotes/myNotes/ViewController.swift
@@ -9,9 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Properties
+    
+    // MARK: Outlets
+    @IBOutlet weak var textViewCampo: UITextView!
+    
+    // MARK: Initialization
+    
+    // MARK: Overrides
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        textViewCampo.text = getNotes()
+    }
+    
+    // MARK: Actions
+    @IBAction func saveNotes(_ sender: Any) {
+        if let texto = textViewCampo.text {
+            UserDefaults.standard.setValue(texto, forKey: "notes")
+        }
+    }
+    
+    // MARK: Methods
+    func getNotes() -> String {
+        if let text = UserDefaults.standard.object(forKey: "notes"){
+            return text as! String
+        }
+        return ""
     }
 
 
